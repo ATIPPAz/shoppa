3ebb6661db140592b39b777a2be4cc903ab85d42
Atippaz <nasakun13201@gmail.com>
2022-05-25 08:48:04 +0700

    change

--- nuxt.config.js
+++ nuxt.config.js
@@ -27,7 +27,7 @@ export default {
   axios: {
     // baseURL: process.env.BASE_URL // Used as fallback if no runtime config is provided
     // credentials: true
-    baseURL: "https://d20d-110-168-54-194.ap.ngrok.io/api",
+    baseURL: "https://cd1c-110-168-54-194.ap.ngrok.io/api",
     //prefix: "/api",
     // proxy: true,
   },
